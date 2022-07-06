b18f280176fb2d8b91ab03e3b8b2d20e66eb5c2e
Jeffrey Zang <66485719+jeffrey-zang@users.noreply.github.com>
2022-07-06 20:18:42 +0000

    Add invite link to about

--- commands/about.js
+++ commands/about.js
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(interaction, client) {
 		const embed = new MessageEmbed({
 			title: '***Meleti***: Greek for study',
-			description: 'Meleti was created at Jamhacks 6, from May 20 2022 to May 22 2022. It was created by Daniel Zhang, Li Feng Yin, Jeffrey Zang, and Jashanpreet Singh. Its purpose is to have all the productivity essentials in one easy-to-access place.',
+			description: 'Meleti was created at Jamhacks 6, from May 20 2022 to May 22 2022. It was created by Daniel Zhang, Li Feng Yin, Jeffrey Zang, and Jashanpreet Singh. Its purpose is to have all the productivity essentials in one easy-to-access place.\n\nLink to invite: https://discord.com/invite/wyPGW6Asbx',
 			color: '#5865f2'
 		})
     await interaction.reply({ embeds: [ embed ] })
